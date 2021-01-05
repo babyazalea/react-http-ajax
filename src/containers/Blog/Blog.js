a94ec96c457be4b75c1601d6e956413021e02a3d
babyazalea <usun12@hotmail.com>
2021-01-05 17:39:36 +0900

    loaded post dynmically

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -43,6 +43,7 @@ class Blog extends Component {
         <Switch>
           <Route path="/new-post" component={NewPost} />
           <Route path="/posts" component={Posts} />
+          <Route path="/" component={Posts} />
         </Switch>
       </div>
     );
