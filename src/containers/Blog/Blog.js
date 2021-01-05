e8aba9dc8ee8297a3a4b4558d848359fa8ad6ff9
babyazalea <usun12@hotmail.com>
2021-01-05 16:09:47 +0900

    using params & Switch

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 // import axios from "axios";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 
 import "./Blog.css";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
+import FullPost from "../../containers/Blog/FullPost/FullPost";
 
 class Blog extends Component {
   render() {
@@ -41,7 +42,10 @@ class Blog extends Component {
           </nav>
         </header>
         <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
+        <Switch>
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
